13c248468fc7a383134da55137a276919b185bc7
Nikita <s0qva69@gmail.com>
2022-04-16 21:57:32 +0700

    A sql script for adding few users was added
    Three test users were added in the 'users' table.

---
+++ src/main/resources/sql/V2__add_users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (username, password, email, first_name, last_name)
+VALUES ('test', 'test', 'test@gmail.com', 'Test', 'Test'),
+       ('test1', 'test1', 'test1@gmail.com', 'Test1', 'Test1'),
+       ('test2', 'test2', 'test2@gmail.com', 'Test2', 'Test2');
