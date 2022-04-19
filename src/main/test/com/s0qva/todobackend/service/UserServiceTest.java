85d808efd519236c26321695b273bd1610a4d50a
Nikita <s0qva69@gmail.com>
2022-04-19 16:04:57 +0700

    Unit tests for UserService were added
    A few unit tests for UserService were created.

--- src/main/test/com/s0qva/todobackend/service/UserServiceTest.java
+++ src/main/test/com/s0qva/todobackend/service/UserServiceTest.java
@@ -1,2 +1,97 @@
-package com.s0qva.todobackend.service;public class UserServiceTest {
+package com.s0qva.todobackend.service;
+
+import com.s0qva.todobackend.dto.user.UserReadingDto;
+import com.s0qva.todobackend.exception.NoSuchUserException;
+import com.s0qva.todobackend.mapper.user.UserMapper;
+import com.s0qva.todobackend.model.User;
+import com.s0qva.todobackend.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith({
+        MockitoExtension.class
+})
+public class UserServiceTest {
+    private UserService userService;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private UserMapper userMapper;
+
+    @BeforeEach
+    void setUp() {
+        this.userService = new UserService(userRepository, userMapper);
+    }
+
+    @Test
+    void itShouldReturnAllUsersAsListOfUserReadingDto() {
+        List<User> receivedUsers = new ArrayList<>();
+        receivedUsers.add(new User());
+        receivedUsers.add(new User());
+        receivedUsers.add(new User());
+
+        List<UserReadingDto> expectedResult = new ArrayList<>();
+        expectedResult.add(new UserReadingDto());
+        expectedResult.add(new UserReadingDto());
+        expectedResult.add(new UserReadingDto());
+
+        when(userRepository.findAll()).thenReturn(receivedUsers);
+        when(userMapper.mapFromUserToUserReadingDto(any(User.class))).thenReturn(new UserReadingDto());
+
+        List<UserReadingDto> actualResult = userService.getAllUsers();
+
+        assertThat(actualResult.size()).isEqualTo(expectedResult.size());
+
+        verify(userRepository, times(1)).findAll();
+        verify(userMapper, times(expectedResult.size())).mapFromUserToUserReadingDto(any(User.class));
+    }
+
+    @Test
+    void itShouldReturnUserAsUserReadingDtoByItsId() {
+        Long id = 1L;
+        User foundUser = new User(id, null, null, null);
+        UserReadingDto expectedResult = new UserReadingDto(id, null, null);
+
+        when(userRepository.findById(id)).thenReturn(Optional.of(foundUser));
+        when(userMapper.mapFromUserToUserReadingDto(foundUser)).thenReturn(expectedResult);
+
+        UserReadingDto actualResult = userService.getUserById(id);
+
+        assertThat(actualResult).isEqualTo(expectedResult)
+                .hasFieldOrPropertyWithValue("id", id);
+
+        verify(userRepository, times(1)).findById(id);
+        verify(userMapper, times(1)).mapFromUserToUserReadingDto(foundUser);
+    }
+
+    @Test
+    void itShouldThrowNoSuchUserExceptionWhenUserWithReceivedIdDoNotExist() {
+        Long id = 1L;
+        String expectedExceptionMessage = "There is no user with id = " + id;
+
+        when(userRepository.findById(id)).thenReturn(Optional.empty());
+
+        NoSuchUserException actualException = assertThrows(NoSuchUserException.class,
+                () -> userService.getUserById(id));
+
+        assertThat(actualException)
+                .isInstanceOf(NoSuchUserException.class)
+                .hasMessage(expectedExceptionMessage);
+
+        verify(userRepository, times(1)).findById(id);
+    }
 }
