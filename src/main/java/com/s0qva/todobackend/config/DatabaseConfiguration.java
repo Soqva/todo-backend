628168e064d185ec2b9149ede12b8b26c0d3eb3f
Nikita <s0qva69@gmail.com>
2022-04-21 18:53:20 +0700

    DatabaseConfiguration was added
    As the Heroku updates database credentials sometimes we need to config
    the app that the app will use updated database credentials
    automatically. That's why DatabaseConfiguration as a Spring bean
    configuration was created. This bean configures a DataSource object
    according to the spring.datasource.url value.

---
+++ src/main/java/com/s0qva/todobackend/config/DatabaseConfiguration.java
@@ -0,0 +1,30 @@
+package com.s0qva.todobackend.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+public class DatabaseConfiguration {
+    @Value("${spring.datasource.url}")
+    private String initialDatabaseUrl;
+
+    @Bean
+    public DataSource dataSource() throws URISyntaxException {
+        HikariConfig config = new HikariConfig();
+        URI databaseUri = new URI(initialDatabaseUrl);
+        String username = databaseUri.getUserInfo().split(":")[0];
+        String password = databaseUri.getUserInfo().split(":")[1];
+        String databaseUrl = "jdbc:postgresql://" + databaseUri.getHost() + ':' + databaseUri.getPort() + databaseUri.getPath();
+        config.setJdbcUrl(databaseUrl);
+        config.setUsername(username);
+        config.setPassword(password);
+        return new HikariDataSource(config);
+    }
+}
