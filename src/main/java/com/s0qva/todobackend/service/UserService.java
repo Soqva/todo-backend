0ae5f816f989989d570592a061f1d8b838385d78
Nikita <s0qva69@gmail.com>
2022-04-26 19:09:29 +0700

    Table 'category' in the database was modified
    Now 'category' table contains a reference to a row in the 'users' table,
    therefore one User have MANY category, but one Category belongs to ONE
    user.
    
    User and Category entities were modified according to the changes.
    
    UserService and AuthService have the Transactional annotation now.

--- src/main/java/com/s0qva/todobackend/service/UserService.java
+++ src/main/java/com/s0qva/todobackend/service/UserService.java
@@ -8,11 +8,13 @@ import com.s0qva.todobackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional
 public class UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
