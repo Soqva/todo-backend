fd228a6e8af7a2f17cd8bfe69802c10505a008f9
Nikita <s0qva69@gmail.com>
2022-04-26 19:03:11 +0700

    Profiles were added
    A few profiles (production, dev) were added. That allow us to use a
    specific configuration (if we develop localy we start docker container
    with our app and it uses 'dev' profile that connects to the local
    database, not remote).

--- src/main/java/com/s0qva/todobackend/config/DatabaseConfiguration.java
+++ src/main/java/com/s0qva/todobackend/config/DatabaseConfiguration.java
@@ -5,12 +5,14 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 import java.net.URI;
 import java.net.URISyntaxException;
 
 @Configuration
+@Profile("production")
 public class DatabaseConfiguration {
     @Value("${spring.datasource.url}")
     private String initialDatabaseUrl;
