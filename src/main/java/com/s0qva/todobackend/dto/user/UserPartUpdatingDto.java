c096775278dd021ea81fbf24d9f898454e220d2a
Nikita <s0qva69@gmail.com>
2022-05-17 13:46:10 +0700

    Validation for UserPartUpdatingDto was added
    When a client sends a PATCH request to patch an existing user the
    validator checks if valid received email.

--- src/main/java/com/s0qva/todobackend/dto/user/UserPartUpdatingDto.java
+++ src/main/java/com/s0qva/todobackend/dto/user/UserPartUpdatingDto.java
@@ -5,12 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class UserPartUpdatingDto {
     private String username;
+
+    @Email(message = "${validatedValue} is not an email")
     private String email;
+
     private String password;
 }
